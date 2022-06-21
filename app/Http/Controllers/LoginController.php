d7636326fa4b48ece117ef55b6933d743ba9c38c
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-06-21 22:37:43 +0600

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,7 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\RegistrationModel;
+
 class LoginController extends Controller
 {
-    //
+    function onRegister(Request $request)
+    {
+
+        $username = $request->input('username');
+        $password = $request->input('password');
+        $userCount = RegistrationModel::where('username', $username)->count();
+
+
+
+        if ($userCount != 0) {
+
+        }
+        else {
+            return 'User Not found';
+        }
+
+    }
 }
