6d5e5c337b1af7a42cae39447d97fabf970ace89
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-06-27 03:39:19 +0600

    jwt return

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\RegistrationModel;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class LoginController extends Controller
 {
@@ -15,7 +17,15 @@ class LoginController extends Controller
         $password = $request->input('password');
         $userCount = RegistrationModel::where(['username' => $username, 'password' => $password])->count();
         if ($userCount == 1) {
-
+            $key = env('TOKEN_KEY');
+            $payload = [
+                'site' => 'http://demo.com',
+                'user' => $username,
+                'iat' => time(),
+                'nbf' => time() + 3600
+            ];
+            $jwt = JWT::encode($payload, $key, 'HS256');
+            return response()->json(['status' => 'login sucessfully', 'token' => $jwt, 'username' => $username]);
         }
         else {
             return "password or username is invalid";
